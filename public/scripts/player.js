9bd61451dd7d692b5a86e904134b103daeb1b3dd
Ismael Lourenço <ilourenco@deloitte.pt>
2019-12-16 17:45:06 +0000

    fav icon added

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -1,8 +1,57 @@
 /* eslint-disable */
+let favAnimation = false
+
 function pageLoad(cb) {
     cb()
     document.getElementById("defaultTab").click();
+    let fav = document.getElementsByClassName('clickFav')[0]
+    fav.addEventListener('click', () => {
+        let star = fav.firstElementChild
+        if (!favAnimation) {
+            favAnimation = true
+            if (star.classList.contains("fa-star")) {
+                setTimeout(function () {
+                    star.classList.remove("fa-star")
+                    star.classList.add("fa-star-o")
+                }, 15)
+                document.getElementById('fav_text').innerText = 'Removed!'
+                fav.lastElementChild.classList.add('info-tog')
+                setTimeout(function () {
+                    fav.lastElementChild.classList.remove('info-tog')
+                    favAnimation = false
+                }, 1000)
+            } else {
+                setTimeout(function () {
+                    star.classList.add('fa-star')
+                    star.classList.remove('fa-star-o')
+                }, 150)
+                document.getElementById('fav_text').innerHTML = '&nbsp;&nbsp;Added!'
+                fav.lastElementChild.classList.add('info-tog')
+                setTimeout(function () {
+                    fav.lastElementChild.classList.remove('info-tog')
+                    favAnimation = false
+                }, 1000)
+            }
+        }
+    })
 }
+
+/* 
+} else {
+    $('.click').addClass('active')
+    $('.click').addClass('active-2')
+    setTimeout(function () {
+        $('span').addClass('fa-star')
+        $('span').removeClass('fa-star-o')
+    }, 150)
+    setTimeout(function () {
+        $('.click').addClass('active-3')
+    }, 150)
+    $('.info').addClass('info-tog')
+    setTimeout(function () {
+        $('.info').removeClass('info-tog')
+    }, 1000)
+} */
 
 function changeTab(e, tabName) {
     // Declare all variables
