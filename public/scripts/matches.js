1ec4207e89b5c4e6c129f9cca2e37f90e00d3443
alexoliveira2000 <57905710+alexoliveira2000@users.noreply.github.com>
2020-01-08 17:40:06 +0000

    cool

--- public/scripts/matches.js
+++ public/scripts/matches.js
@@ -1,4 +1,17 @@
 /* eslint-disable */
 function pageLoad(cb) {
-    cb()
+    api.post(window.location.pathname).then(res => {
+        loadData(res.data)
+        cb()
+    }).catch(err => console.log(err))
+}
+
+function loadData(data) {
+    let team1 = data.team1
+    let team2 = data.team2
+    let maps = data.maps
+    let team1Players = data.players.team1
+    let team2Players = data.players.team2
+
+    document.getElementById("teste").innerHTML = team1;
 }
