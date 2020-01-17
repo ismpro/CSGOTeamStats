90a5396bbf07ef5b3591fd0296af17bad30bc1f5
Ismael Lourenço <ilourenco@deloitte.pt>
2020-01-17 15:45:26 +0000

    search v1

--- app/routes/search.js
+++ app/routes/search.js
@@ -4,13 +4,12 @@ const Players = require('../models/Players.js');
 module.exports = function () {
     return function (req, res) {
         let data = req.body;
-        console.log(data)
         let teamsPromise = Teams.find({
             name: {
                 '$regex': `^${data.text}`,
                 '$options': 'i'
             }
-        })
+        }).limit(3).exec()
         let playerPromise = Players.find({
             $or: [{
                 name: {
@@ -23,11 +22,23 @@ module.exports = function () {
                     '$options': 'i'
                 }
             }]
-        })
+        }).limit(3).exec()
         Promise.all([teamsPromise, playerPromise]).then(data => {
+            let players = data[1];
+            let teams = data[0];
             res.status(200).send({
-                teams: data[0],
-                players: data[1]
+                teams: teams.map((team) => ({
+                    id: team.id,
+                    name: team.name,
+                    logo: team.logo,
+                })),
+                players: players.map((player) => ({
+                    id: player.id,
+                    ign: player.ign,
+                    name: player.name,
+                    image: player.image,
+                    team: player.team,
+                }))
             })
         }).catch(err => {
             console.log(err)
@@ -35,31 +46,3 @@ module.exports = function () {
         })
     }
 }
-
-/*
-@TODO
-Alterar a query para que retorne so 100 limitado pode haver 1000 de players por exemplo e isso levaria um error de memoria
-Exemplo:
-In the latest mongoose (3.8.1 at the time of writing), you do two things differently: (1) you have to pass single argument to sort(),
-which must be an array of constraints or just one constraint, and (2) execFind() is gone,
-and replaced with exec() instead. Therefore, with the mongoose 3.8.1 you'd do this:
-
-var q = Post.find({published: true}).sort({'date': -1}).limit(20);
-q.exec(function(err, posts) {
-     // `posts` will be of length 20
-});
-
-or you can chain it together simply like that:
-
-Post
-.find({published: true})
-.sort({'date': -1})
-.limit(20)
-.exec(function(err, posts) {
-     // `posts` will be of length 20
-});
-
-ou:
-
-let users = await Users.find({}, null,{limit: 50});
-*/
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,6 +8,7 @@ function pageLoad(cb) {
 function search(e) {
     e.preventDefault();
     let input = document.getElementById('search_string').value;
+    input = input.trim()
     if (input.length > 0 && !searching) {
         searching = true
         api.post('/search', {
@@ -17,38 +18,77 @@ function search(e) {
             if (res.status === 200) {
                 let players = res.data.players
                 let teams = res.data.teams
-                let ul = document.getElementById('search_list');
-                ul.innerHTML = ""
-                if (players.length !== 0) {
-                    let li1 = document.createElement('li')
-                    li1.innerHTML = '<p>Players:</p>'
-                    ul.appendChild(li1);
-                    for (const player of players) {
-                        let li = document.createElement('li');
-                        let a = document.createElement('a');
-                        a.href = `/player/${player.id}`
-                        a.innerHTML = player.ign
-                        li.appendChild(a)
-                        ul.appendChild(li)
+                let searchDiv = document.getElementById('search_list');
+                searchDiv.innerHTML = ""
+                if (players.length === 0 && teams.length === 0) {
+                    searchDiv.innerHTML = '<p>No results</p>'
+                } else {
+                    if (players.length !== 0) {
+                        let div1 = document.createElement('div')
+                        div1.classList.add('search-header')
+                        div1.innerHTML = '<span><b>Players</b></span>'
+                        searchDiv.appendChild(div1);
+                        for (const player of players) {
+                            let divPlayer = document.createElement('div');
+                            divPlayer.classList.add('searchBox')
+                            let a = document.createElement('a');
+                            a.href = `/player/${player.id}`
+                            let img = document.createElement('img')
+                            img.src = player.image
+                            img.classList.add('search-img')
+                            a.appendChild(img)
+                            divPlayer.appendChild(a)
+                            let divInfo = document.createElement('div')
+                            divInfo.classList.add('search-info')
+                            let aName = document.createElement('a')
+                            aName.classList.add('search-text')
+                            let aTeam = document.createElement('a')
+                            aTeam.classList.add('search-text')
+                            aName.innerHTML = `${player.name || ''} "${player.ign || ''}"`
+                            aTeam.innerHTML = player.team ? `Playing for ${player.team.name || ''}` : 'No team'
+                            aName.href = `/player/${player.id}`
+                            aTeam.href = player.team ? `/team/${player.team.id}` : '#'
+                            divInfo.appendChild(aName)
+                            divInfo.appendChild(aTeam)
+                            divPlayer.appendChild(divInfo)
+                            searchDiv.appendChild(divPlayer)
+                        }
                     }
-                }
-                if (teams.length !== 0) {
-                    let li1 = document.createElement('li')
-                    li1.innerHTML = '<p>Teams:</p>'
-                    ul.appendChild(li1);
-                    for (const team of teams) {
-                        let li = document.createElement('li');
-                        let a = document.createElement('a');
-                        a.href = `/team/${team.id}`
-                        a.innerHTML = team.name
-                        li.appendChild(a)
-                        ul.appendChild(li)
+                    if (teams.length !== 0) {
+                        let div2 = document.createElement('div')
+                        div2.classList.add('search-header')
+                        div2.innerHTML = '<span><b>Teams</b></span>';
+                        searchDiv.appendChild(div2);
+                        for (const team of teams) {
+                            let divPlayer = document.createElement('div');
+                            divPlayer.classList.add('searchBox')
+                            let a = document.createElement('a');
+                            a.href = `/team/${team.id}`
+                            let img = document.createElement('img')
+                            img.src = team.logo
+                            img.classList.add('search-img')
+                            a.appendChild(img)
+                            divPlayer.appendChild(a)
+                            let divInfo = document.createElement('div')
+                            divInfo.classList.add('search-info')
+                            let aName = document.createElement('a')
+                            aName.classList.add('search-text')
+                            aName.innerHTML = team.name
+                            aName.href = `/team/${team.id}`
+                            divInfo.appendChild(aName)
+                            divPlayer.appendChild(divInfo)
+                            searchDiv.appendChild(divPlayer)
+                        }
                     }
                 }
             }
         }).catch(err => {
-
+            let searchDiv = document.getElementById('search_list');
+            searchDiv.innerHTML = "<p>Server Error</p>"
         })
+    } else if (input.length === 0) {
+        let searchDiv = document.getElementById('search_list');
+        searchDiv.innerHTML = "<p>Type to search</p>"
     }
 }
 
