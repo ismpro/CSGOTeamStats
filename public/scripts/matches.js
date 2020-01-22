929a57601533ddd11ed14399e3056bd64f4e8777
alexoliveira2000 <57905710+alexoliveira2000@users.noreply.github.com>
2020-01-22 16:27:00 +0000

    matches

--- public/scripts/matches.js
+++ public/scripts/matches.js
@@ -7,11 +7,29 @@ function pageLoad(cb) {
 }
 
 function loadData(data) {
-    let team1 = data.team1
-    let team2 = data.team2
+    let team1Name = data.team1.name
+    let team2Name = data.team2.name
     let maps = data.maps
     let team1Players = data.players.team1
     let team2Players = data.players.team2
+    let statsTeam1 = data.playerStats.team1
+    let statsTeam2 = data.playerStats.team2
 
-    document.getElementById("teste").innerHTML = team1;
+    let logo_team1 = "https://static.hltv.org/images/team/logo/" + data.team1.id
+    let logo_team2 = "https://static.hltv.org/images/team/logo/" + data.team2.id
+
+    document.getElementById('team1_logo').src = logo_team1
+    document.getElementById('team2_logo').src = logo_team2
+
+    document.getElementById('team1_name').innerHTML = team1Name
+    document.getElementById('team2_name').innerHTML = team2Name
+
+    // preencher a parte da estatística
+    stats = "<table>"
+    for (const player of statsTeam1) {
+        
+    }
+    stats += "</table>"
+
+
 }
