0204eb516b4b6f9747b785f7eb7d330063676505
alexoliveira2000 <57905710+alexoliveira2000@users.noreply.github.com>
2020-01-24 11:53:21 +0000

    matches

--- app/routes/match-post.js
+++ app/routes/match-post.js
@@ -1,4 +1,38 @@
 const Match = require('../models/Match');
+const Players = require('../models/Players');
+const {
+    byCountry
+} = require('country-code-lookup')
+
+const getPlayer = async function (players) {
+    let parsedPlayer = [];
+    for (const player of players) {
+        let playerInfo = await Players.findOne({
+            id: player.id
+        })
+        if (playerInfo) {
+            parsedPlayer.push({
+                id: playerInfo.id,
+                name: playerInfo.ign,
+                fullName: playerInfo.name,
+                image: playerInfo.image,
+                country: playerInfo.country
+            })
+        } else {
+            parsedPlayer.push({
+                id: player.id,
+                name: 'Not Specify',
+                fullName: 'Not Specify',
+                image: 'https://csgoteamstats.herokuapp.com/static/images/unimage-player.svg',
+                country: {
+                    code: 'world',
+                    name: 'world',
+                }
+            })
+        }
+    }
+    return parsedPlayer
+}
 
 module.exports = function (api) {
     return function (req, res) {
@@ -7,7 +41,14 @@ module.exports = function (api) {
             id: id
         }, function (err, match) {
             if (!err) {
-                res.status(200).json(match)
+                //res.status(200).json(match)                
+                Promise.all([getPlayer(match.players.team1), getPlayer(match.players.team2)]).then(data => {
+                    res.status(200).json({
+                        match: match,
+                        playersTeam1: data[0],
+                        playersTeam2: data[1]
+                    })
+                })
             } else {
                 res.status(500).send('Error on server! Try again later!')
             }
--- public/scripts/matches.js
+++ public/scripts/matches.js
@@ -7,29 +7,114 @@ function pageLoad(cb) {
 }
 
 function loadData(data) {
+    // nomes das equipas
     let team1Name = data.team1.name
     let team2Name = data.team2.name
+    document.getElementById('team1_name').innerHTML = team1Name
+    document.getElementById('team2_name').innerHTML = team2Name
+    // imagens das equipas
+    let logo_team1 = "https://static.hltv.org/images/team/logo/" + data.team1.id
+    let logo_team2 = "https://static.hltv.org/images/team/logo/" + data.team2.id
+    document.getElementById('team1_logo').src = logo_team1
+    document.getElementById('team2_logo').src = logo_team2
+    // mapas
     let maps = data.maps
-    let team1Players = data.players.team1
+    // resultado
+    let leftCounter = 0
+    let rightCounter = 0
+    // para cada mapa, vai aumentando o contador de cada equipa
+    for (const map of maps) {
+        let leftResult = parseInt(map.result.substr(0, map.result.indexOf(':')))
+        let rightResult = parseInt(map.result.substr(map.result.indexOf(':') + 1, map.result.indexOf(' ') - 2))
+
+        if (leftResult) {
+            if (leftResult > rightResult) {
+                leftCounter++
+            } else {
+                rightCounter++
+            }
+        }
+    }
+    document.getElementById('result_team1').innerHTML = leftCounter
+    document.getElementById('result_team2').innerHTML = rightCounter
+    // imagens, nomes e resultados dos mapas
+    let i = 1
+    for (const map of maps) {
+        document.getElementById("map" + i).style.display = "block";
+        let mapImage = "https://hltv.org/img/static/maps/"
+        let mapName
+        if (map.name === "nuke") {
+            mapImage += "nuke"
+            mapName = "Nuke"
+        } else if (map.name === "trn") {
+            mapImage += "train"
+            mapName = "Train"
+        } else if (map.name === "mrg") {
+            mapImage += "mirage"
+            mapName = "Mirage"
+        } else if (map.name === "d2") {
+            mapImage += "dust2"
+            mapName = "Dust2"
+        } else if (map.name === "ovp") {
+            mapImage += "overpass"
+            mapName = "Overpass"
+        } else if (map.name === "vertigo") {
+            mapImage += "vertigo"
+            mapName = "Vertigo"
+        } else if (map.name === "inf") {
+            mapImage += "inferno"
+            mapName = "Inferno"
+        }
+        mapImage += ".png"
+        let team1Score = map.result.substr(0, map.result.indexOf(':'))
+        let team2Score = map.result.substr(map.result.indexOf(':') + 1, map.result.indexOf(' ') - 2)
+        // se não jogaram algum mapa que estava na lista, fica com menos opacidade
+        if (!team1Score) {
+            document.getElementById("map" + i).style.opacity = "0.5";
+        }
+
+        document.getElementById("map" + i + "_image").src = mapImage
+        document.getElementById("map" + i + "_name").innerHTML = mapName
+        document.getElementById("map" + i + "_team1_score").innerHTML = team1Score
+        document.getElementById("map" + i + "_team2_score").innerHTML = team2Score
+        i++
+    }
+
+    console.log(data.playersTeam1)
+    
+    //let team1Players = data.players.team1
+    /*
+    i = 1
+    for (player of team1Players) {
+        console.log(player.age)
+        /*
+        let playerStats = "<td>"
+        playerStats+="<img src=\"https://static.hltv.org/images/bigflags/30x20/" + player.country.code + ".gif\" class=\"player_flag\">"
+        playerStats+="<td>" + player.ign + "</td>"
+        
+
+        document.getElementById("player" + i).innerHTML = playerStats
+        */
+        /*
+        let playerSecondImgTag = document.createElement('img')
+        playerSecondImgTag.src = "https://www.countryflags.io/" + player.country.code + "/shiny/24.png"
+
+        document.getElementById("player" + i).appendChild(playerSecondImgTag)
+        i++
+        
+    }
+
+    
     let team2Players = data.players.team2
     let statsTeam1 = data.playerStats.team1
     let statsTeam2 = data.playerStats.team2
-
-    let logo_team1 = "https://static.hltv.org/images/team/logo/" + data.team1.id
-    let logo_team2 = "https://static.hltv.org/images/team/logo/" + data.team2.id
-
-    document.getElementById('team1_logo').src = logo_team1
-    document.getElementById('team2_logo').src = logo_team2
-
-    document.getElementById('team1_name').innerHTML = team1Name
-    document.getElementById('team2_name').innerHTML = team2Name
-
+    
     // preencher a parte da estatística
     stats = "<table>"
     for (const player of statsTeam1) {
-        
+
     }
     stats += "</table>"
 
-
+    */
 }
